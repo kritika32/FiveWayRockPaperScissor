838a2d3d3809badb6978969884e9a1f123ab9be6
kritika32 <59146229+kritika32@users.noreply.github.com>
2021-02-05 22:57:22 +0530

    Final update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = require('./router')
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', express.static('public'));
